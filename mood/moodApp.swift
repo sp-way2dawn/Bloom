81078ce553697ee5eb877ee033209728a98a677a
Sophia <shpark@brynmawr.edu>
2024-11-09 10:28:16 -0500

    Initial Commit

---
+++ mood/moodApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodApp.swift
+//  mood
+//
+//  Created by Sophia Park on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct moodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moodTests/moodTests.swift
@@ -0,0 +1,17 @@
+//
+//  moodTests.swift
+//  moodTests
+//
+//  Created by Sophia Park on 11/9/24.
+//
+
+import Testing
+@testable import mood
+
+struct moodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
